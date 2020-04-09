6378b61ee4b25858152b0d7f654013146d7aa812
boris <boris.shaporov1@gmail.com>
2020-04-09 13:24:37 +0300

    Change log output

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,12 +1,21 @@
 import TextFragments.Text;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 
 public class Main {
+    private static final Logger LOG = LogManager.getLogger(Main.class);
+
+
     public static void main(String[] args) {
+        LOG.info("Main started");
+
         TextParser textParser = new TextParser("src/main/Text ENG.txt");
         Text text;
         text = textParser.parse();
         System.out.println(text);
         System.out.println(text.sortByCountingSymbols(Input.getChar()));
+
+        LOG.info("Main finished");
     }
 }
